b1305ebc88d44773d49168550b1511e3260c620e
binyamin.regev <beniregev@gmail.com>
2023-02-17 11:06:20 +0200

    Refactoring - moving to mappers package

--- src/main/java/com/beniregev/diabetes/mappers/DiaryEntryMapper.java
+++ src/main/java/com/beniregev/diabetes/mappers/DiaryEntryMapper.java
@@ -1,8 +1,9 @@
-package com.beniregev.diabetes.model;
+package com.beniregev.diabetes.mappers;
 
 import com.beniregev.diabetes.dtos.DiaryEntryDto;
 import com.beniregev.diabetes.dtos.DiaryEntryPostRequest;
 import com.beniregev.diabetes.dtos.DiaryEntryPostResponse;
+import com.beniregev.diabetes.model.DiaryEntry;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
@@ -18,5 +19,7 @@ public interface DiaryEntryMapper {
     DiaryEntry diaryEntryPostRequestToDiaryEntry(DiaryEntryPostRequest source);
     DiaryEntryPostResponse diaryEntryToDiaryEntryPostResponse(DiaryEntry source);
 
-    List<DiaryEntryDto> toListDiaryEntryDto(List<DiaryEntry> listDiaryEntries);
+    List<DiaryEntryDto> listDiaryEntryPostRequestToListDiaryEntryDto(List<DiaryEntryPostRequest> listDiaryEntriesPostRequest);
+    List<DiaryEntryDto> listDiaryEntryToListDiaryEntryDto(List<DiaryEntry> listDiaryEntries);
+    List<DiaryEntry> listDiaryEntryDtoToListDiaryEntry(List<DiaryEntryDto> listDiaryEntryDto);
 }
