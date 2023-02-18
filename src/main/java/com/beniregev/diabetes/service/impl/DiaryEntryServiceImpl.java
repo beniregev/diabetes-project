3d49a2c77c21701308adad3a0223554fc0e49b9c
binyamin.regev <beniregev@gmail.com>
2023-02-18 21:01:30 +0200

    Adding 2 methods for adding multiple/bulk of DiaryEntries

--- src/main/java/com/beniregev/diabetes/service/impl/DiaryEntryServiceImpl.java
+++ src/main/java/com/beniregev/diabetes/service/impl/DiaryEntryServiceImpl.java
@@ -4,7 +4,7 @@ import com.beniregev.diabetes.dtos.DiaryEntryDto;
 import com.beniregev.diabetes.dtos.DiaryEntryPostRequest;
 import com.beniregev.diabetes.enums.MeasurementEnum;
 import com.beniregev.diabetes.model.DiaryEntry;
-import com.beniregev.diabetes.model.DiaryEntryMapper;
+import com.beniregev.diabetes.mappers.DiaryEntryMapper;
 import com.beniregev.diabetes.repository.DiaryEntryRepository;
 import com.beniregev.diabetes.service.DiaryEntryService;
 import org.springframework.stereotype.Service;
@@ -26,50 +26,80 @@ public class DiaryEntryServiceImpl implements DiaryEntryService {
         this.diaryEntryRepository = diaryEntryRepository;
     }
 
+    @Override
     public List<DiaryEntry> getAll() {
         return diaryEntryRepository.findAll();
     }
 
+    @Override
     public DiaryEntry getById(final int id) {
         return diaryEntryRepository.findById(id).orElse(null);
     }
 
+    @Override
     public List<DiaryEntry> getDiaryEntriesToday() {
         LocalDateTime from = LocalDateTime.of(TODAY, LocalTime.MIDNIGHT);
         return diaryEntryRepository.findDiaryEntriesByDateTimeBetween(from, EOD_TODAY);
     }
 
+    @Override
     public List<DiaryEntry> getDiaryEntriesLast7Days() {
         LocalDateTime from = LocalDateTime.of(TODAY.minusDays(7L), LocalTime.MIDNIGHT);
         return diaryEntryRepository.findDiaryEntriesByDateTimeBetween(from, EOD_TODAY);
     }
 
+    @Override
     public List<DiaryEntry> getDiaryEntriesLast14Days() {
         LocalDateTime from = LocalDateTime.of(TODAY.minusDays(14L), LocalTime.MIDNIGHT);
         return diaryEntryRepository.findDiaryEntriesByDateTimeBetween(from, EOD_TODAY);
     }
 
+    @Override
     public List<DiaryEntry> getDiaryEntriesLast30Days() {
         LocalDateTime from = LocalDateTime.of(TODAY.minusDays(30L), LocalTime.MIDNIGHT);
         return diaryEntryRepository.findDiaryEntriesByDateTimeBetween(from, EOD_TODAY);
     }
 
+    @Override
     public List<DiaryEntry> getDiaryEntriesLast90Days() {
         LocalDateTime from = LocalDateTime.of(TODAY.minusDays(90L), LocalTime.MIDNIGHT);
         return diaryEntryRepository.findDiaryEntriesByDateTimeBetween(from, EOD_TODAY);
     }
 
+    @Override
     public Map<String, String> getMapNameDescription() {
         return MeasurementEnum.mapDescriptions;
     }
 
+    @Override
     public String getMeasurementDescriptionByName(final String when) {
         return MeasurementEnum.mapDescriptions.get(when);
     }
 
+    @Override
     public DiaryEntryDto create(final DiaryEntryPostRequest newEntry) {
         DiaryEntry created = diaryEntryRepository.save(DiaryEntryMapper.INSTANCE.diaryEntryPostRequestToDiaryEntry(newEntry));
         return DiaryEntryMapper.INSTANCE.diaryEntryToDiaryEntryDto(created);
+    }
+
+    @Override
+    public List<DiaryEntryDto> create(List<DiaryEntryPostRequest> listDiaryEntriesPostRequest) {
+        List<DiaryEntryDto> listDiaryEntryDto = DiaryEntryMapper.INSTANCE.listDiaryEntryPostRequestToListDiaryEntryDto(listDiaryEntriesPostRequest);
+        List<DiaryEntry> listDiaryEntry = DiaryEntryMapper.INSTANCE.listDiaryEntryDtoToListDiaryEntry(listDiaryEntryDto);
+        List<DiaryEntry> listCreated = diaryEntryRepository.saveAll(listDiaryEntry);
+        return DiaryEntryMapper.INSTANCE.listDiaryEntryToListDiaryEntryDto(listCreated);
+    }
+
+    @Override
+    public List<DiaryEntryDto> create(LocalDate defaultDate, List<DiaryEntryPostRequest> listDiaryEntriesPostRequest) {
+        List<DiaryEntry> listDiaryEntry = listDiaryEntriesPostRequest.stream()
+                .map(e -> new DiaryEntry(
+                        LocalDateTime.of(defaultDate, e.getMeasurement().getDefaultTime()),
+                        e.getMeasurement(),
+                        e.getResult()))
+                .toList();
+        List<DiaryEntry> listCreated = diaryEntryRepository.saveAll(listDiaryEntry);
+        return DiaryEntryMapper.INSTANCE.listDiaryEntryToListDiaryEntryDto(listCreated);
     }
 
     public void deleteAll() {
